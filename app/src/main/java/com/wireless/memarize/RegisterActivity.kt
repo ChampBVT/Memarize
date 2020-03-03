60285512cbb51462ad64f1128d8fbb081b0c4eae
champbvt <champ51951@gmail.com>
2020-03-03 22:48:58 +0700

    fix register page and main page

--- app/src/main/java/com/wireless/memarize/RegisterActivity.kt
+++ app/src/main/java/com/wireless/memarize/RegisterActivity.kt
@@ -1,4 +1,37 @@
 package com.wireless.memarize
 
-class RegisterActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var createBtn: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        auth = FirebaseAuth.getInstance()
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        createBtn = findViewById(R.id.createButton)
+        createBtn.setOnClickListener {}
+        auth.createUserWithEmailAndPassword(email.toString(), password.toString()).addOnCompleteListener(this, OnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Toast.makeText(this, "Successfully Registered as $email and $password", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else {
+                Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
 }
