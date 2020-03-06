d3339b1c56615c986852a25d9d58667b39a0dbf5
unknown <chaitawat.bos@student.mahidol.ac.th>
2020-03-06 18:30:23 +0700

    login activity

--- app/src/main/java/com/wireless/memarize/LoginActivity.kt
+++ app/src/main/java/com/wireless/memarize/LoginActivity.kt
@@ -2,17 +2,53 @@ package com.wireless.memarize
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var registerBtn: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEt: EditText
+    private lateinit var passwordEt: EditText
+    private lateinit var loginBttn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        emailEt = findViewById(R.id.email_et)
+        passwordEt = findViewById(R.id.password_et)
+        loginBttn = findViewById(R.id.login_bttn)
         registerBtn = findViewById(R.id.register)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginBttn.setOnClickListener{
+            Toast.makeText(this, "Login Button pressed", Toast.LENGTH_LONG).show()
+            var email: String = emailEt.text.toString()
+            var password: String = passwordEt.text.toString()
+            if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
+                Toast.makeText(this@LoginActivity, "Please fill in all the fields", Toast.LENGTH_LONG).show()
+            }else{
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else{
+                        Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+        }
+
         registerBtn.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
