27c02b8defda8563ccfeafd702f831814d11dc7e
champbvt <borvorntat.nir@gmail.com>
2020-03-06 19:17:24 +0700

    Modified registerAct to show an error

--- app/src/main/java/com/wireless/memarize/RegisterActivity.kt
+++ app/src/main/java/com/wireless/memarize/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.wireless.memarize
 import android.R.attr.key
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -46,6 +47,9 @@ class RegisterActivity : AppCompatActivity() {
                         startActivity(intent)
                         finish()
                     } else {
+                        Log.d("test", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "User Authentication Failed: " + (task.exception?.message
+                            ?: "No error message"), Toast.LENGTH_SHORT).show();
                         Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
                     }
                 })
