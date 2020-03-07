3510774211572620481b1e6fec0f77cd475ec558
champbvt <champ51951@gmail.com>
2020-03-07 21:36:29 +0700

    Added realtime database with read and write operations

--- app/src/main/java/com/wireless/memarize/MainActivity.kt
+++ app/src/main/java/com/wireless/memarize/MainActivity.kt
@@ -1,46 +1,37 @@
 package com.wireless.memarize
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var signOutBtn: Button
+    private lateinit var textHeader: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        var login = false
-//        var email = "null"
-//        var password = "null"
-//        val bundle = intent.getBundleExtra("Bundle")
-//        if (bundle !== null) {
-//            email = bundle.getString("Email", "null")
-//            password = bundle.getString("Password", "null")
-//            login = bundle.getBoolean("Login", false)
-//        }
-//        Toast.makeText(
-//            this,
-//            "Current status\nLogin: $login\nEmail: $email\nPassword: $password",
-//            Toast.LENGTH_LONG
-//        ).show()
+
+        textHeader = findViewById(R.id.welcomeHeader)
         signOutBtn = findViewById(R.id.signOut)
         auth = FirebaseAuth.getInstance()
         val user = auth.currentUser;
-        if (user == null) { goToLoginIntent() }
-        else {
-            val email = user.email
-            val uid = user.uid
-            Toast.makeText(
-                this,
-                "Current status\nName: AS\nEmail: $email\nPassword: $uid",
-                Toast.LENGTH_LONG
-            ).show()
+        if (user == null) {
+            goToLoginIntent()
+        } else {
+            getUserInfo(user.uid)
             signOutBtn.setOnClickListener {
                 auth.signOut()
                 goToLoginIntent()
@@ -48,10 +39,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun goToLoginIntent(){
+    private fun goToLoginIntent() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun getUserInfo(uid: String) {
+        val database = FirebaseDatabase.getInstance().reference
+        var userName: String = "No Found"
+        var email: String = "No Found"
+        database.child("users").child(uid).addListenerForSingleValueEvent(
+            object : ValueEventListener {
+                @SuppressLint("SetTextI18n")
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val user = dataSnapshot.getValue(User::class.java)
+                    if (user != null) {
+                        userName = user.name
+                        email = user.email
+                        textHeader.text = "Welcome $userName"
+                        showToast(userName, email, uid)
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
+                }
+
+            })
+    }
+
+    private fun showToast(userName:String, email:String, uid: String){
+        Toast.makeText(
+            this,
+            "Current status \nName: $userName\nEmail: $email\nuid: $uid",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
 
--- app/src/main/java/com/wireless/memarize/RegisterActivity.kt
+++ app/src/main/java/com/wireless/memarize/RegisterActivity.kt
@@ -1,6 +1,5 @@
 package com.wireless.memarize
 
-import android.R.attr.key
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
@@ -11,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -18,6 +18,8 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var createBtn: Button
+    private lateinit var name: EditText
+    private lateinit var database: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,44 +27,55 @@ class RegisterActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
+        name = findViewById(R.id.name)
         createBtn = findViewById(R.id.create)
 
         createBtn.setOnClickListener {
-            val emailText = email.text.toString()
-            val passwordText = password.text.toString()
-            if(TextUtils.isEmpty(emailText)|| TextUtils.isEmpty(passwordText)){
-                Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show()
-            } else {
-                auth.createUserWithEmailAndPassword(emailText, passwordText)
-                    .addOnCompleteListener(this, OnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Toast.makeText(
-                                this,
-                                "Successfully Registered as $emailText\nPassword: $passwordText",
-                                Toast.LENGTH_LONG
-                            ).show()
-                            val intent = Intent(this, MainActivity::class.java).apply {
-                                val bundle = Bundle()
-                                bundle.putString("Email", emailText)
-                                bundle.putString("Password", passwordText)
-                                bundle.putBoolean("Login", true)
-                                putExtra("Bundle", bundle)
-                            }
-                            val closeLoginActivity = Intent("Close_Login_Activity")
-                            sendBroadcast(closeLoginActivity)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            Log.d("test", "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(
-                                this, "User Authentication Failed: " + (task.exception?.message
-                                    ?: "No error message"), Toast.LENGTH_SHORT
-                            ).show();
-                            Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
-                        }
-                    })
-            }
+            emailRegister(email.text.toString(), password.text.toString())
         }
     }
 
+    private fun emailRegister(emailText:String, passwordText:String){
+        createBtn.isEnabled = false
+        if(TextUtils.isEmpty(emailText)|| TextUtils.isEmpty(passwordText)){
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show()
+            createBtn.isEnabled = true
+        } else {
+            auth.createUserWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, OnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        createUser(name.text.toString(),emailText)
+                        Toast.makeText(
+                            this,
+                            "Successfully Registered as $emailText\nPassword: $passwordText",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        val closeLoginActivity = Intent("Close_Login_Activity")
+                        sendBroadcast(closeLoginActivity)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Log.d("test", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this, "Failed: " + (task.exception?.message
+                                ?: "No error message"), Toast.LENGTH_SHORT
+                        ).show();
+                        createBtn.isEnabled = true
+                    }
+                })
+        }
+    }
+
+    private fun createUser(name:String, email:String){
+        val database = FirebaseDatabase.getInstance().reference
+        val user = User(name, email)
+        database.child("users").child(auth.currentUser?.uid.toString()).setValue(user)
+            .addOnCompleteListener(this, OnCompleteListener { task ->
+            Toast.makeText(
+                this, "Failed: " + (task.exception?.message
+                    ?: "No error message"), Toast.LENGTH_SHORT
+            ).show();
+        });
+    }
 }
