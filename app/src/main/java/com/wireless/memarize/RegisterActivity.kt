46a89fde4c03f7ce50191dcb9caf02a1bfc7f609
champbvt <borvorntat.nir@gmail.com>
2020-04-02 23:40:31 +0700

    successfully add pet name pet type in the registration

--- app/src/main/java/com/wireless/memarize/RegisterActivity.kt
+++ app/src/main/java/com/wireless/memarize/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.wireless.memarize
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Base64
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -17,9 +18,8 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var email: EditText
     private lateinit var password: EditText
-    private lateinit var createBtn: Button
+    private lateinit var nextBtn: Button
     private lateinit var name: EditText
-    private lateinit var database: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,54 +28,62 @@ class RegisterActivity : AppCompatActivity() {
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
         name = findViewById(R.id.name)
-        createBtn = findViewById(R.id.create)
+        nextBtn = findViewById(R.id.next)
 
-        createBtn.setOnClickListener {
-            emailRegister(email.text.toString(), password.text.toString())
+        nextBtn.setOnClickListener {
+            //emailRegister(email.text.toString(), password.text.toString())
+            val bundle = Bundle()
+            bundle.putString("email", email.text.toString())
+            val encode: String = Base64.encodeToString(password.text.toString().toByteArray(), Base64.DEFAULT)
+            bundle.putString("password", encode)
+            bundle.putString("name", name.text.toString())
+            val intent = Intent(this, RegisterActivity2::class.java)
+            intent.putExtra("regInfo", bundle)
+            startActivity(intent)
         }
     }
 
-    private fun emailRegister(emailText:String, passwordText:String){
-        createBtn.isEnabled = false
-        if(TextUtils.isEmpty(emailText)|| TextUtils.isEmpty(passwordText)){
-            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show()
-            createBtn.isEnabled = true
-        } else {
-            auth.createUserWithEmailAndPassword(emailText, passwordText)
-                .addOnCompleteListener(this, OnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        createUser(name.text.toString(),emailText)
-                        Toast.makeText(
-                            this,
-                            "Successfully Registered as $emailText\nPassword: $passwordText",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        val intent = Intent(this, MainActivity::class.java)
-                        val closeLoginActivity = Intent("Close_Login_Activity")
-                        sendBroadcast(closeLoginActivity)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        Log.d("test", "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            this, "Failed: " + (task.exception?.message
-                                ?: "No error message"), Toast.LENGTH_SHORT
-                        ).show();
-                        createBtn.isEnabled = true
-                    }
-                })
-        }
-    }
-
-    private fun createUser(name:String, email:String){
-        val database = FirebaseDatabase.getInstance().reference
-        val user = User(name, email)
-        database.child("users").child(auth.currentUser?.uid.toString()).setValue(user)
-            .addOnCompleteListener(this, OnCompleteListener { task ->
-            Toast.makeText(
-                this, "Failed: " + (task.exception?.message
-                    ?: "No error message"), Toast.LENGTH_SHORT
-            ).show();
-        });
-    }
+//    private fun emailRegister(emailText:String, passwordText:String){
+//        createBtn.isEnabled = false
+//        if(TextUtils.isEmpty(emailText)|| TextUtils.isEmpty(passwordText)){
+//            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show()
+//            createBtn.isEnabled = true
+//        } else {
+//            auth.createUserWithEmailAndPassword(emailText, passwordText)
+//                .addOnCompleteListener(this, OnCompleteListener { task ->
+//                    if (task.isSuccessful) {
+//                        createUser(name.text.toString(),emailText)
+//                        Toast.makeText(
+//                            this,
+//                            "Successfully Registered as $emailText\nPassword: $passwordText",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                        val intent = Intent(this, MainActivity::class.java)
+//                        val closeLoginActivity = Intent("Close_Login_Activity")
+//                        sendBroadcast(closeLoginActivity)
+//                        startActivity(intent)
+//                        finish()
+//                    } else {
+//                        Log.d("test", "createUserWithEmail:failure", task.exception)
+//                        Toast.makeText(
+//                            this, "Failed: " + (task.exception?.message
+//                                ?: "No error message"), Toast.LENGTH_SHORT
+//                        ).show();
+//                        createBtn.isEnabled = true
+//                    }
+//                })
+//        }
+//    }
+//
+//    private fun createUser(name:String, email:String){
+//        val database = FirebaseDatabase.getInstance().reference
+//        val user = User(name, email)
+//        database.child("users").child(auth.currentUser?.uid.toString()).setValue(user)
+//            .addOnCompleteListener(this, OnCompleteListener { task ->
+//            Toast.makeText(
+//                this, "Failed: " + (task.exception?.message
+//                    ?: "No error message"), Toast.LENGTH_SHORT
+//            ).show();
+//        });
+//    }
 }
--- app/src/main/java/com/wireless/memarize/User.kt
+++ app/src/main/java/com/wireless/memarize/User.kt
@@ -2,5 +2,9 @@ package com.wireless.memarize
 
 data class User(
     var name: String = "",
-    var email: String = ""
+    var email: String = "",
+    var petName: String = "",
+    var petType: String = "",
+    var coins: Long = 0,
+    var progress: String =""
 )
